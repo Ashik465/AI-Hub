d580d766c1a07f554a6f2cea5bec7864516d18e5
Ashik465 <helloashikul@gmail.com>
2023-03-03 04:10:19 +0600

    dynamic-card-done

--- js/ai.js
+++ js/ai.js
@@ -24,20 +24,34 @@ data.forEach(aiDetails => {
     const div =document.createElement('div');
     div.classList.add("card", "w-full", "bg-base-100", "shadow-xl","p-5","border" )
 
-    const {image,features, name} =aiDetails ;
+    const {image,features, name,published_in} =aiDetails ;
 
     div.innerHTML=` <figure><img class="rounded-lg " src=${image} alt="Shoes" /></figure>
     <h1 class="text-2xl font-bold my-2">Features</h1>
 
-    <div class="mb-4">
+    <div class="mb-4 text-[#585858]">
                     <p>1.${features[0]}</p>
                     <p>2.${features[1]}</p>
                     <p>3.${features[2]}</p>
                    </div>
 
                    <hr>
-
-                   <h1 class="text-2xl font-bold my-2">${name}</h1>
+                   <div class="flex  items-center place-content-between">
+                   <div>
+                     <h1 class="text-2xl font-bold my-4">${name}</h1>
+                     <div class="flex flex-row gap-3">
+                       <div class=" text-[#585858]"><i class="fa-solid fa-calendar-days"></i></div>
+                       <div><h1 class="text-base text-[#585858]">${published_in}</h1></div>
+                       
+                     </div>
+                   </div>
+       
+                   <div >
+                     <span class="text-[#EB5757] p-5 bg-red-50 rounded-full">
+                       <i class="fa-solid fa-arrow-right"></i
+                     ></span>
+                   </div>
+                 </div>
     
     ` ;
 
