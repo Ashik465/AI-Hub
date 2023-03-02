0e62a69f37974be4da9a7561b2473a80c9ae07d3
Ashik465 <helloashikul@gmail.com>
2023-03-03 01:52:53 +0600

    display-card

--- js/ai.js
+++ js/ai.js
@@ -6,8 +6,43 @@ const loadAI = async () => {
     
     const data = await res.json() ;
 
-    console.log(data.data );
+    displayAI(data.data.tools.slice(0,6) );
 
 }
+
+// display data 
+
+const displayAI =  (data) => {
+
+// console.log(data );
+
+const container =document.getElementById('card-container');
+
+data.forEach(aiDetails => {
+    // console.log(aiDetails);
+
+    const div =document.createElement('div');
+    div.classList.add("card", "w-96", "bg-base-100", "shadow-xl")
+
+    div.innerHTML=` <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+    <div class="card-body">
+      <h2 class="card-title">Shoes!</h2>
+      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+      </div>
+    </div> ` ;
+
+    container.appendChild(div);
+
+
+});
+
+
+
+
+}
+
+
 
 loadAI();
